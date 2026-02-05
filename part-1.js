90f0c58ec1754ff9892c8b29ff2b666afe7e9e32
Mira-2234 <tasmimmira822@gmail.com>
2026-02-05 19:40:30 +0600

    js problem solving

---
+++ part-1.js
@@ -0,0 +1,134 @@
+// দুটি সংখ্যা যোগ করার একটি প্রোগ্রাম লেখো:
+
+
+let a = 20;
+let b = 45;
+const sum = a+b;
+console.log(sum);
+
+// দুটি সংখ্যার বিয়োগ, গুণ ও ভাগ বের করো:
+
+
+let x = 654;
+let y = 987;
+const total = x-y;
+console.log(total);
+
+// This is *
+
+let y = 34;
+let z = 43;
+const total = y*z;
+console.log(total);
+
+// This is / 
+
+let x = 36;
+let y = 6;
+const total = x/y;
+console.log(total);
+
+
+// একটি সংখ্যা even না odd চেক করো:
+
+
+// This is odd number problem solving
+
+
+
+let number = 7;
+if(number % 2 === 0){
+    console.log("It's even number");
+} else{
+    console.log("It's odd number")
+}
+
+
+// This is even number problem solving
+
+
+let number = 64;
+if(number % 2 === 0){
+    console.log("It's even number");
+} else{
+    console.log("It's odd number")
+}
+
+
+
+
+
+// একটি সংখ্যা positive, negative না zero চেক করো
+
+
+let num = 24;
+if (num > 0){
+    console.log("This is positive Number");
+} else if(num < 0){
+    console.log("This is Negative number");
+} else{
+    console.log("This is zero");
+}
+
+
+
+let num = -24;
+if (num > 0){
+    console.log("This is positive Number");
+} else if(num < 0){
+    console.log("This is Negative number");
+} else{
+    console.log("This is zero");
+}
+
+
+
+let num = 0;
+if (num > 0){
+    console.log("This is positive Number");
+} else if(num < 0){
+    console.log("This is Negative number");
+} else{
+    console.log("This is zero");
+}
+
+
+// তিনটি সংখ্যার গড় (average) বের করো:
+
+let a = 78;
+let b = 65;
+let c = 95;
+
+const sum = a + b + c;
+const count = 3;
+const total = sum / count;
+console.log(total);
+
+
+// Celsius থেকে Fahrenheit convert করো
+
+const celcius = 92;
+const fahrenheit = (92 * 9/5)+32;
+console.log(fahrenheit);
+
+
+// Rectangle এর area বের করো
+
+const lenth = 5;
+width = 10;
+
+const area = lenth * width;
+console.log(area);
+
+
+// একটি সংখ্যা 3 এবং 5 উভয় দিয়ে divisible কিনা চেক করো:
+
+
+// let num = 25;
+ let num = 15;
+if(num % 3 === 0 && num % 5 === 0){
+    console.log(true);
+}
+else {
+    console.log(false);
+}
